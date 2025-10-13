e5cbfc63539c9b8472f511c112a1e03be09bba2d
Shrey Kothari <shrey.gk1@gmail.com>
2025-10-13 12:39:12 -0700

    attempted to add 10 pixel buffer

--- DataVis.py
+++ DataVis.py
@@ -8,9 +8,13 @@ def routeDisplay(route, input_basename, D):     #route are the corrodinates in a
     distance_traveled = ceil(D)   
     x_coordinates = [nodeCoordinates[0] for nodeCoordinates in route]
     y_coordinates = [nodeCoordinates[1] for nodeCoordinates in route]
+    xAxisMin, xAxisMax = min(x_coordinates), max(x_coordinates)
+    yAxisMin, yAxisMax = min(y_coordinates), max(y_coordinates)
 
-    plt.figure(facecolor = 'lightblue')
-    plt.plot(x_coordinates, y_coordinates, marker = "o")
-    plt.scatter(x_coordinates[0], y_coordinates[0], color = 'red')   #the start and the end nodes of the line graph
-    plt.title("Optimal Drone Route")
-    plt.savefig(f"{input_basename}_SOLUTION_{distance_traveled}.jpg")
+    plt.figure(facecolor = 'lightblue')                                #background
+    plt.plot(x_coordinates, y_coordinates, marker = "o")               
+    plt.scatter(x_coordinates[0], y_coordinates[0], color = 'red')     #the start and the end nodes of the line graph
+    plt.title("Optimal Drone Route")    
+    plt.xlim(xAxisMin - 10, xAxisMax + 10)                             #adding 10 pixel buffer between any point and the edges
+    plt.ylim(yAxisMin - 10, yAxisMax + 10)        
+    plt.savefig(f"{input_basename}_SOLUTION_{distance_traveled}.jpg")   #saving it as a jpeg file
