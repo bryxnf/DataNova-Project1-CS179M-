4f06d08573a266442f85702ef10216234d481c80
Shrey Kothari <shrey.gk1@gmail.com>
2025-10-13 12:28:16 -0700

    Changes to routeFileCreator; started on DataVis.py

--- DataVis.py
+++ DataVis.py
@@ -1,4 +1,16 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+from math import ceil
+import os
 
-def routeDisplay(route):
-    return 0
+def routeDisplay(route, input_basename, D):     #route are the corrodinates in an array ex: [(1,2), (2, 4)]
+    
+    distance_traveled = ceil(D)   
+    x_coordinates = [nodeCoordinates[0] for nodeCoordinates in route]
+    y_coordinates = [nodeCoordinates[1] for nodeCoordinates in route]
+
+    plt.figure(facecolor = 'lightblue')
+    plt.plot(x_coordinates, y_coordinates, marker = "o")
+    plt.scatter(x_coordinates[0], y_coordinates[0], color = 'red')   #the start and the end nodes of the line graph
+    plt.title("Optimal Drone Route")
+    plt.savefig(f"{input_basename}_SOLUTION_{distance_traveled}.jpg")
--- tspOutputFileMaker.py
+++ tspOutputFileMaker.py
@@ -4,7 +4,7 @@ from math import ceil
 def routeFileCreator(path, input_basename, D, out_dir):
 
     distance_traveled = ceil(D)                                        #rounding D up because D may have decimals
-    filename = f"{input_basename}_SOLUTIONS_{distance_traveled}.txt"
+    filename = f"{input_basename}_SOLUTION_{distance_traveled}.txt"
     filepath = os.path.join(out_dir, filename)
 
     with open(filepath, "w") as pathTextFile:                           #creating the textfile
