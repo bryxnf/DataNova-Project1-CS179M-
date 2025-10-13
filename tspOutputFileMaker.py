586cf46d0c991a169e3500276d3d7bcd421ed1cf
Shrey Kothari <shrey.gk1@gmail.com>
2025-10-12 17:20:55 -0700

    creating route output file

---
+++ tspOutputFileMaker.py
@@ -0,0 +1,14 @@
+import os
+from math import ceil
+
+def routeFileCreator(path, input_basename, D, out_dir):
+
+    distance_traveled = ceil(D)                                        #rounding D up because D may have decimals
+    filename = f"{input_basename}_SOLUTIONS_{distance_traveled}.txt"
+    filepath = os.path.join(out_dir, filename)
+
+    with open(filepath, "w") as pathTextFile:                           #creating the textfile
+        for nodeNumber in path:
+            pathTextFile.write(f"{nodeNumber}\n")
+
+    return filepath
