1ac465db8d5dcc6972226e278eff244bb88388c3
bryxnf <bfran029@ucr.edu>
2025-10-17 20:56:19 +0000

    Attempting to call algorithm, although waiting on the parameters.

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ def main():
     input_folder = "InputCordsFolder"
     file_path = os.path.join(input_folder, filename)
 
-    if not os.path.exists(file_path):
+    if not os.path.exists(file_path): #incase file doesnt exist
         print(f"[ERROR] '{file_path}' not found.")
         return
 
-    print(f"[Info] Found input file: {file_path}")
+    #print(f"Drop locations found within input file: {file_path}")
+
+    print("Running Genetic Algorithm...")
+    best_route, total_distance = run_algorithm(file_path) #assigns local variable to the returned tupleand the total distance
+    results = run_algorithm(file_path)
+
+    if results is None: #incase algorithm fails
+        raise RuntimeError("Genetic algorithm failed to produce a result.")
+    best_route, total_distance = results
+
+    print(f"Algorithm completed. Distance traveled: {total_distance:.2f}")
 
 
 
