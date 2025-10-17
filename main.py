3d6c474f8595a656d2444dace45385ee41f46784
bryxnf <bfran029@ucr.edu>
2025-10-17 17:36:33 +0000

    Accidently added to main

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import os
-import sys
-import argparse
-import numpy as np
-
-from geneticAlg import run_algorithm
-from tspOutputFileMaker import routeFileCreator
-from DataVis import routeDisplay
-
-def main():
-    print("===Drone Route Optimization Program===")
-    print("[Info] Basic setup loaded successfully.")
-
-if __name__ == "__main__":
-    main()
 
